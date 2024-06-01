cc46b7fc8d14b69322703d9ec01cc2e6f5ce680f
sakshi <sakshiyewale23@gmail.com>
2024-06-01 14:05:29 +0530

    01-06

--- lib/controller/order_controller.dart
+++ lib/controller/order_controller.dart
@@ -1,43 +1,43 @@
 import 'dart:convert';
-
 import 'package:get/get.dart';
+import 'package:http/http.dart' as http;
 
-import '../api/model/section_model.dart';
-import 'package:http/http.dart'as http;
+class OrderController extends GetxController {
+  var data = [].obs;
+  var data1 = [].obs;
+  var isLoading = false.obs;
+  var isLoadingTables = false.obs;
+  var errorMessage = ''.obs;
 
-class OrderController extends GetxController
-{
-  // List<Model1>abc=[];
-  // Future<List<Model1>> getApi() async {
-  //   final response=await http.get(Uri.parse("http://localhost:5000/api/section/"));
-  //   var data=jsonDecode(response.body.toString());
-  //   if(response.statusCode==200)
-  //   {
-  //     for(var index in data)
-  //     {
-  //       abc.add(Model1.fromJson(index));
-  //     }
-  //     return abc;
-  //   }
-  //   else{
-  //     return abc;
-  //   }
-
-  var data;
-
-  Future<void> getApi()async{
-    final response=await http.get(Uri.parse("http://localhost:5000/api/section/"));
-    print('111');
-    if(response.statusCode==200)
-    {
-      data=jsonDecode(response.body.toString());
-      print('222');
-      print("#############$data");
-      return;
-    }
-    else{
-
+  Future<void> getApi() async {
+    isLoading.value = true;
+    try {
+      final response = await http.get(Uri.parse("http://localhost:5000/api/section/"));
+      if (response.statusCode == 200) {
+        data.value = jsonDecode(response.body.toString());
+      } else {
+        errorMessage.value = 'Failed to load sections';
+      }
+    } catch (e) {
+      errorMessage.value = 'Error: $e';
+    } finally {
+      isLoading.value = false;
     }
   }
 
+  Future<void> getTablesForSection(String section) async {
+    isLoadingTables.value = true;
+    try {
+      final response = await http.get(Uri.parse("http://localhost:5000/api/table/tables?section=$section"));
+      if (response.statusCode == 200) {
+        data1.value = jsonDecode(response.body.toString());
+      } else {
+        errorMessage.value = 'Failed to load tables';
+      }
+    } catch (e) {
+      errorMessage.value = 'Error: $e';
+    } finally {
+      isLoadingTables.value = false;
+    }
+  }
 }
--- lib/screens/order_screen.dart
+++ lib/screens/order_screen.dart
@@ -2,176 +2,167 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:my_hotel/controller/order_controller.dart';
 import 'package:sizer/sizer.dart';
+import '../utils/app_colors.dart';
+import 'data_screen.dart';
+// Import the new screen
 
 class OrderScreen extends StatefulWidget {
-  const OrderScreen({super.key});
+  const OrderScreen({Key? key}) : super(key: key);
 
   @override
   State<OrderScreen> createState() => _OrderScreenState();
 }
 
 class _OrderScreenState extends State<OrderScreen> {
-OrderController orderController =Get.find();
-int selectedIndex = 0;
+  OrderController orderController = Get.find();
+  int selectedIndex = 0;
+  int? selectedTableIndex;
+
+  @override
+  void initState() {
+    super.initState();
+    orderController.getApi();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Column(
         children: [
-          // FutureBuilder(
-          //     future: orderController.getApi(),
-          //     builder: (context,snapshot)
-          // {
-          //   if(snapshot.connectionState==ConnectionState.waiting)
-          //     {
-          //       return Center(child: CircularProgressIndicator());
-          //     }
-          //   else{
-          //     return Expanded(
-          //       child: ListView.builder(
-          //           itemCount: orderController.data.length,
-          //           itemBuilder: (context,index)
-          //
-          //       {
-          //         bool isSelected = selectedIndex==0;
-          //         return Row(
-          //           children: [
-          //             Padding(
-          //               padding: EdgeInsets.all(8.0),
-          //               child: Container(
-          //                 height: 6.5.h,
-          //                 width: 36.w,
-          //                 decoration: BoxDecoration(
-          //                   color: isSelected
-          //                       ? Colors.white
-          //                       : Colors.white,
-          //                   borderRadius: BorderRadius.circular(15),
-          //                   border: Border.all(color: Colors.grey, width: 2),
-          //                 ),
-          //                 child: Row(
-          //                   children: [
-          //                     GestureDetector(
-          //                       child: Padding(
-          //                         padding: EdgeInsets.only(left: 10.sp),
-          //                         child: Container(
-          //                           decoration: BoxDecoration(
-          //                               borderRadius:
-          //                               BorderRadius.circular(50),
-          //                               color: isSelected
-          //                                   ? Colors.orange
-          //                                   : Colors.white,
-          //                               border: Border.all(
-          //                                   color: isSelected
-          //                                       ? Colors.orange
-          //                                       : Colors.grey,
-          //                                   width: 2)),
-          //                           height: 2.3.h,
-          //                           width: 4.6.w,
-          //                         ),
-          //                       ),
-          //                       onTap: () {
-          //                         setState(() {
-          //                           selectedIndex = 0;
-          //                         });
-          //                       },
-          //                     ),
-          //                     Padding(
-          //                       padding: EdgeInsets.only(left: 7.sp),
-          //                       child: Text(orderController.data[index]["name"].toString()), // Display item from the list
-          //                     )
-          //                   ],
-          //                 ),
-          //               ),
-          //             ),
-          //           ],
-          //         );
-          //       }),
-          //     );
-          //   }
-          // })
           Container(
             height: 10.h,
             width: 100.w,
-            child: FutureBuilder(
-              future: orderController.getApi(),
-              builder: (context,snapshot)
-              {
-                if(snapshot.connectionState==ConnectionState.waiting)
-                  {
-                    return CircularProgressIndicator();
-                  }
-                else{
-                  return Expanded(
-                    child: ListView.builder(
-                      scrollDirection: Axis.horizontal,
-                      shrinkWrap: true,
-                      itemCount:orderController.data.length, // Use the length of items list
-                      itemBuilder: (context, index) {
-                        bool isSelected = selectedIndex==0;
-                        return Row(
-                          children: [
-                            Padding(
-                              padding: EdgeInsets.all(8.0),
-                              child: Container(
-                                height: 6.5.h,
-                                width: 30.w,
-                                decoration: BoxDecoration(
-                                  color: isSelected
-                                      ? Colors.white
-                                      : Colors.white,
-                                  borderRadius: BorderRadius.circular(15),
-                                  border: Border.all(color: Colors.grey, width: 2),
-                                ),
-                                child: Row(
-                                  children: [
-                                    GestureDetector(
-                                      child: Padding(
-                                        padding: EdgeInsets.only(left: 10.sp),
-                                        child: Container(
-                                          decoration: BoxDecoration(
-                                              borderRadius:
-                                              BorderRadius.circular(50),
-                                              color: isSelected
-                                                  ? Colors.orange
-                                                  : Colors.white,
-                                              border: Border.all(
-                                                  color: isSelected
-                                                      ? Colors.orange
-                                                      : Colors.grey,
-                                                  width: 2)),
-                                          height: 2.3.h,
-                                          width: 4.6.w,
-                                        ),
-                                      ),
-                                      onTap: () {
-                                        setState(() {
-                                          selectedIndex = 0;
-                                        });
-                                      },
-                                    ),
-                                    Padding(
-                                      padding: EdgeInsets.only(left: 7.sp),
-                                      child: Text(orderController.data[index]['name'].toString()) // Dsplay item from the list
-                                    )
-                                  ],
-                                ),
-                              ),
-                            ),
-                          ],
-                        );
-                      },
-                    ),
-                  );
-                }
-              },
-            )
+            child: Obx(() {
+              if (orderController.data.isEmpty) {
+                return Center(child: CircularProgressIndicator());
+              } else {
+                return ListView.builder(
+                  scrollDirection: Axis.horizontal,
+                  itemCount: orderController.data.length,
+                  itemBuilder: (context, index) {
+                    return _buildCategoryButton(
+                      orderController.data[index]['name'].toString(),
+                      index,
+                    );
+                  },
+                );
+              }
+            }),
           ),
+          SizedBox(height: 20),
+          // Display MainHall, ACHall, or Selected Category
+          selectedIndex == 0 ? _MainHall() : _ACHall(),
         ],
       ),
     );
   }
+
+  Widget _MainHall() {
+    return Expanded(
+      child: Obx(() {
+        if (orderController.data1.isEmpty) {
+          return Center(child: CircularProgressIndicator());
+        } else {
+          return GridView.builder(
+            padding: EdgeInsets.all(10),
+            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 2,
+              crossAxisSpacing: 10.sp,
+              mainAxisSpacing: 10.sp,
+            ),
+            itemCount: orderController.data1.length,
+            itemBuilder: (context, index) {
+              return _buildTableWidget(orderController.data1[index]["tableName"].toString(), index);
+            },
+          );
+        }
+      }),
+    );
+  }
+
+  Widget _ACHall() {
+    return Expanded(
+      child: Obx(() {
+        if (orderController.data1.isEmpty) {
+          return Center(child: CircularProgressIndicator());
+        } else {
+          return GridView.builder(
+            padding: EdgeInsets.all(10),
+            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 2,
+              crossAxisSpacing: 10.sp,
+              mainAxisSpacing: 10.sp,
+            ),
+            itemCount: orderController.data1.length,
+            itemBuilder: (context, index) {
+              return _buildTableWidget(orderController.data1[index]["tableName"].toString(), index);
+            },
+          );
+        }
+      }),
+    );
+  }
+
+  Widget _buildTableWidget(String tableName, int index) {
+    bool isSelected = selectedTableIndex == index;
+    Color containerColor = isSelected ? Colors.orangeAccent : Colors.white;
+
+    return GestureDetector(
+      onTap: () {
+        setState(() {
+          selectedTableIndex = index;
+        });
+        // Navigate to the TableDetailsScreen when a table is selected
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) => TableDetailsScreen(tableName: tableName),
+          ),
+        );
+      },
+      child: Container(
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(10),
+          color: containerColor,
+          border: Border.all(color: Colors.grey),
+        ),
+        child: Center(
+          child: Text(tableName),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildCategoryButton(String category, int index) {
+    bool isSelected = selectedIndex == index;
+    Color containerColor = isSelected ? Colors.orangeAccent : ColorsForApp.whiteColor;
+
+    return GestureDetector(
+      onTap: () {
+        setState(() {
+          selectedIndex = index;
+          selectedTableIndex = null; // Reset table selection when category changes
+        });
+        // Fetch tables for the selected category
+        orderController.getTablesForSection(category);
+      },
+      child: Container(
+        padding: EdgeInsets.all(10),
+        margin: EdgeInsets.symmetric(horizontal: 4.0),
+        decoration: BoxDecoration(
+          border: Border.all(color: isSelected ? Colors.orangeAccent : ColorsForApp.blackColor),
+          borderRadius: BorderRadius.circular(5),
+          color: containerColor,
+        ),
+        child: Text(
+          category,
+          style: TextStyle(
+            fontSize: 9.sp,
+            fontWeight: FontWeight.w500,
+            color: isSelected ? ColorsForApp.whiteColor : ColorsForApp.blackColor,
+          ),
+        ),
+      ),
+    );
+  }
 }
-
-
-
-
